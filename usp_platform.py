569053fa7d4c4a28a0a56ae0f60d25c8129231fc
Florent Kermarrec <florent@enjoy-digital.fr>
2026-01-20 13:18:28 +0100

    Add usp test platform/target that will be use for dev.

---
+++ usp_platform.py
@@ -0,0 +1,84 @@
+from litex.build.generic_platform import *
+from litex.build.xilinx         import XilinxUSPPlatform
+from litex.build.xilinx         import VivadoProgrammer
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+    # Clk / Rst.
+    ("clk200", 0,
+        Subsignal("p", Pins("T24")),
+        Subsignal("n", Pins("U24")),
+        IOStandard("DIFF_SSTL12")
+    ),
+
+    # Serial.
+    ("serial", 0,
+        Subsignal("rx", Pins("C12"), IOStandard("LVCMOS33")), # GPIO-0.
+        Subsignal("tx", Pins("E12"), IOStandard("LVCMOS33")), # GPIO-1.
+    ),
+
+    # LEDs.
+    ("user_led", 0, Pins("H12"), IOStandard("LVCMOS33")),
+    ("user_led", 1, Pins("J12"), IOStandard("LVCMOS33")),
+    ("user_led", 2, Pins("H13"), IOStandard("LVCMOS33")),
+    ("user_led", 3, Pins("G14"), IOStandard("LVCMOS33")),
+    ("user_led", 4, Pins("J13"), IOStandard("LVCMOS33")),
+    ("user_led", 5, Pins("H14"), IOStandard("LVCMOS33")),
+    ("user_led", 6, Pins("J14"), IOStandard("LVCMOS33")),
+    ("user_led", 7, Pins("J15"), IOStandard("LVCMOS33")),
+
+    # GPIOs.
+    ("gpio", 0, Pins("C12"), IOStandard("LVCMOS33")),
+    ("gpio", 1, Pins("E12"), IOStandard("LVCMOS33")),
+    ("gpio", 2, Pins("C13"), IOStandard("LVCMOS33")),
+    ("gpio", 3, Pins("C14"), IOStandard("LVCMOS33")),
+    ("gpio", 4, Pins("B12"), IOStandard("LVCMOS33")),
+    ("gpio", 5, Pins("E13"), IOStandard("LVCMOS33")),
+    ("gpio", 6, Pins("D13"), IOStandard("LVCMOS33")),
+    ("gpio", 7, Pins("D14"), IOStandard("LVCMOS33")),
+
+    # PCIe.
+    ("pcie_x4", 0,
+        Subsignal("rst_n", Pins("AC13"), IOStandard("LVCMOS33")),
+        Subsignal("wake",  Pins("AC14"), IOStandard("LVCMOS33")),
+        Subsignal("pedet", Pins("AD14"), IOStandard("LVCMOS33")),
+        Subsignal("clk_p", Pins("AB7")),
+        Subsignal("clk_n", Pins("AB6")),
+        Subsignal("rx_p",  Pins("AF2 AE4 AD2 AB2")),
+        Subsignal("rx_n",  Pins("AF1 AE3 AD1 AB1")),
+        Subsignal("tx_p",  Pins("AF7 AE9 AD7 AC5")),
+        Subsignal("tx_n",  Pins("AF6 AE8 AD6 AC4")),
+    ),
+
+    # PCIe.
+    ("pcie_clk", 0,
+        Subsignal("p", Pins("AB7")),
+        Subsignal("n", Pins("AB6")),
+    ),
+]
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(XilinxUSPPlatform):
+    default_clk_name   = "clk200"
+    default_clk_period = 1e9/200e6
+
+    def __init__(self, toolchain="vivado"):
+        XilinxUSPPlatform.__init__(self, "xcau10p-ffvb676-2-i", _io, toolchain=toolchain)
+
+    def create_programmer(self):
+        return VivadoProgrammer()
+
+    def do_finalize(self, fragment):
+        XilinxUSPPlatform.do_finalize(self, fragment)
+
+        # Timings Constraints.
+        self.add_period_constraint(self.lookup_request("clk200",        loose=True), 1e9/200e6)
+        self.add_period_constraint(self.lookup_request("pcie_x4:clk_p", loose=True), 1e9/100e6)
+
+        # Bitstreams Parameters.
+        self.add_platform_command("set_property BITSTREAM.GENERAL.COMPRESS TRUE     [current_design]")
+        self.add_platform_command("set_property BITSTREAM.CONFIG.CONFIGRATE 51.0    [current_design]")
+        self.add_platform_command("set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4     [current_design]")
+        self.add_platform_command("set_property BITSTREAM.CONFIG.UNUSEDPIN PULLNONE [current_design]")
---
+++ usp_target.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+import os
+
+from migen import *
+from litex.gen import *
+
+from usp_platform import Platform
+
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder  import *
+
+from litex.soc.cores.clock import USMMCM, USIDELAYCTRL
+from litex.soc.cores.led   import LedChaser
+
+from litedram.modules import MT40A1G8
+from litedram.phy     import usddrphy
+
+from litepcie.phy.usppciephy import USPPCIEPHY
+
+from litescope import LiteScopeAnalyzer
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class _CRG(LiteXModule):
+    def __init__(self, platform, sys_clk_freq):
+        self.rst       = Signal()
+        self.cd_sys    = ClockDomain()
+        self.cd_sys4x  = ClockDomain()
+        self.cd_idelay = ClockDomain()
+
+        # Clk.
+        clk200 = platform.request("clk200")
+
+        # PLL.
+        self.pll = pll = USMMCM(speedgrade=-2)
+        self.comb += pll.reset.eq(self.rst)
+        pll.register_clkin(clk200, 200e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq, with_reset=False)
+        platform.add_false_path_constraints(self.cd_sys.clk, pll.clkin)
+
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCCore):
+    def __init__(self, sys_clk_freq=int(125e6), with_led_chaser=True, **kwargs):
+        # Platform ---------------------------------------------------------------------------------
+        platform = Platform()
+
+        # CRG --------------------------------------------------------------------------------------
+        self.crg = _CRG(platform, sys_clk_freq)
+
+        # SoCCore ----------------------------------------------------------------------------------
+        kwargs["uart_name"] = "stub"
+        SoCCore.__init__(self, platform, sys_clk_freq, ident="LiteNVME Test SoC.",**kwargs)
+
+        # UARTBone ---------------------------------------------------------------------------------
+        self.add_uartbone()
+
+        # PCIe -------------------------------------------------------------------------------------
+        with_pcie = True
+        if with_pcie:
+            pcie_pads = platform.request("pcie_x4")
+            pcie_pads_rst_n = pcie_pads.rst_n
+            pcie_pads.rst_n = Signal()
+            self.pcie_phy = USPPCIEPHY(platform, pcie_pads,
+                speed      = "gen3",
+                data_width = 128,
+                ip_name    = "pcie4c_uscale_plus",
+                bar0_size  = 0x20000,
+                mode       = "RootPort",
+            )
+            self.add_pcie(phy=self.pcie_phy)
+
+            # Set manual locations to avoid Vivado to remap lanes.
+            platform.toolchain.pre_placement_commands.append("reset_property LOC [get_cells -hierarchical -filter {{NAME=~*pcie_usp_i/*GTHE4_CHANNEL_PRIM_INST}}]")
+            platform.toolchain.pre_placement_commands.append("set_property LOC GTHE4_CHANNEL_X0Y3 [get_cells -hierarchical -filter {{NAME=~*pcie_usp_i/*gthe4_channel_gen.gen_gthe4_channel_inst[0].GTHE4_CHANNEL_PRIM_INST}}]")
+            platform.toolchain.pre_placement_commands.append("set_property LOC GTHE4_CHANNEL_X0Y2 [get_cells -hierarchical -filter {{NAME=~*pcie_usp_i/*gthe4_channel_gen.gen_gthe4_channel_inst[1].GTHE4_CHANNEL_PRIM_INST}}]")
+            platform.toolchain.pre_placement_commands.append("set_property LOC GTHE4_CHANNEL_X0Y1 [get_cells -hierarchical -filter {{NAME=~*pcie_usp_i/*gthe4_channel_gen.gen_gthe4_channel_inst[2].GTHE4_CHANNEL_PRIM_INST}}]")
+            platform.toolchain.pre_placement_commands.append("set_property LOC GTHE4_CHANNEL_X0Y0 [get_cells -hierarchical -filter {{NAME=~*pcie_usp_i/*gthe4_channel_gen.gen_gthe4_channel_inst[3].GTHE4_CHANNEL_PRIM_INST}}]")
+
+            # RstN Generation.
+            from litex.gen.genlib.misc import WaitTimer
+            self.rst_n_timer       = WaitTimer(2      * sys_clk_freq) # 2s   period.
+            self.rst_n_timer_pulse = WaitTimer(100e-3 * sys_clk_freq) # 10ms pulse.
+            self.comb += [
+                self.rst_n_timer.wait.eq(       ~self.rst_n_timer.done),
+                self.rst_n_timer_pulse.wait.eq( ~self.rst_n_timer.done),
+                pcie_pads_rst_n.eq(self.rst_n_timer_pulse.done),
+                pcie_pads.rst_n.eq(self.rst_n_timer_pulse.done),
+            ]
+            self.comb += [
+                platform.request("user_led", 1).eq(self.rst_n_timer_pulse.done),
+                platform.request("user_led", 2).eq(self.pcie_phy._link_status.fields.status),
+                platform.request("user_led", 3).eq(self.pcie_phy._link_status.fields.phy_down),
+            ]
+
+    # LiteScope Probes (Debug) -----------------------------------------------------------------
+
+    # PCIe.
+    def add_pcie_probe(self):
+        self.pcie_phy.add_ltssm_tracer()
+        analyzer_signals = [
+            # Rst.
+            self.pcie_phy.pcie_rst_n,
+
+            # Link Status.
+            self.pcie_phy._link_status.fields.rate,
+            self.pcie_phy._link_status.fields.width,
+            self.pcie_phy._link_status.fields.ltssm
+        ]
+        self.analyzer = LiteScopeAnalyzer(analyzer_signals,
+            depth        = 4096,
+            clock_domain = "sys",
+            register     = True,
+            csr_csv      = "analyzer.csv"
+        )
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    from litex.build.parser import LiteXArgumentParser
+    parser = LiteXArgumentParser(platform=Platform, description="LiteNVME Test SoC.")
+    parser.add_target_argument("--sys-clk-freq", default=125e6, type=float, help="System clock frequency.")
+    args = parser.parse_args()
+
+    soc = BaseSoC(sys_clk_freq=args.sys_clk_freq, **parser.soc_argdict)
+    soc.add_pcie_probe()
+
+    builder = Builder(soc, **parser.builder_argdict)
+    if args.build:
+        builder.build(**parser.toolchain_argdict)
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(builder.get_bitstream_filename(mode="sram"))
+
+if __name__ == "__main__":
+    main()
